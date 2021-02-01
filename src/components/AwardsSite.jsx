7e69eadc60c1e32df84a85456a7731bf3cd89b32
Arek Cichocki <arexspeed95@gmail.com>
2021-02-01 11:44:28 +0100

    add vote status

--- src/components/AwardsSite.jsx
+++ src/components/AwardsSite.jsx
@@ -5,6 +5,33 @@ import "../styles/Awards.scss";
 
 
 const AwardsSite = () => {
+  const [votesStatus, setVoteStatus] = useState(false)
+  const [voten, setVoten] = useState({
+    goalkeeper: '',
+    defender: '',
+    forward: ''
+  })
+
+  const voteSend = (e) => {
+    e.preventDefault()
+    setVoteStatus(true)
+    console.log('All Votes', voten)
+  }
+
+  const voteGoalkeeper = (e) => {
+    setVoten({...voten, goalkeeper: e.target.value})
+    console.log(voten)
+  }
+
+  const voteDefender = (e) => {
+    setVoten({...voten, defender: e.target.value})
+    console.log(voten)
+  }
+
+  const voteForward = (e) => {
+    setVoten({...voten, forward: e.target.value})
+    console.log(voten)
+  }
   return (
     <>
       <section>
@@ -13,52 +40,58 @@ const AwardsSite = () => {
         </div>
         <div className="container">
         <header className="tv__header">Vote for the best players</header>
-          <div className="awards__boxes">
+          <form className="awards__boxes" onSubmit={voteSend}>
           <div className="awards__box">
             <h2 className="awards__title">
               Goalkeepers
             </h2>
-            <form>
               {votes.filter(vote => vote.category === "Goalkeeper")
               .map(vote => (
                 <div className="awards__checkbox">
-                  <input type="radio" name="goalkeeper" id={vote.name} value={vote.name} />
+                  <input type="radio" name="goalkeeper" id={vote.name} value={vote.name} onClick={voteGoalkeeper} />
                   <label for={vote.name} className="span"><span>{vote.name}</span></label>
                 </div>
-              ))}
-            </form>
+              ))}     
           </div>
 
           <div className="awards__box">
             <h2 className="awards__title">
               Defenders
             </h2>
-            <form>
+            
               {votes.filter(vote => vote.category === "Defender")
               .map(vote => (
-                <p>
-                  <input type="radio" name="defender" id={vote.name} value={vote.name} />
-                  <label for={vote.name}>{vote.name}</label>
-                </p>
+                <div className="awards__checkbox">
+                <input type="radio" name="defender" id={vote.name} value={vote.name} onClick={voteDefender} />
+                <label for={vote.name} className="span"><span>{vote.name}</span></label>
+              </div>
               ))}
-            </form>
+            
           </div>
 
           <div className="awards__box">
             <h2 className="awards__title">
               Forwards
             </h2>
-            <form>
-              {votes.filter(vote => vote.category === "Forward")
+            
+            {votes.filter(vote => vote.category === "Forward")
               .map(vote => (
-                <p>
-                  <input type="radio" name="forward" id={vote.name} value={vote.name} />
-                  <label for={vote.name}>{vote.name}</label>
-                </p>
+                <div className="awards__checkbox">
+                <input type="radio" name="forward" id={vote.name} value={vote.name} onClick={voteForward} />
+                <label for={vote.name} className="span"><span>{vote.name}</span></label>
+              </div>
               ))}
-            </form>
+            
           </div>
-          </div>
+          <button type="submit">Vote</button>
+          </form>
+
+          {votesStatus && 
+          <div>
+          <p>GK: {voten.goalkeeper}</p>
+          <p>DF: {voten.defender}</p>
+          <p>FW: {voten.forward}</p>
+          </div>}
           </div>
       </section>
     </>
