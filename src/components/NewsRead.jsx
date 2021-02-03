153ce339b6c62290e0ef99833486b174095996d1
Arek Cichocki <arexspeed95@gmail.com>
2021-02-03 12:37:06 +0100

    news read new style

--- src/components/NewsRead.jsx
+++ src/components/NewsRead.jsx
@@ -2,29 +2,58 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import {news} from '../data/news'
 import Footer from './Footer'
+import '../styles/NewsRead.scss'
 
 const NewsRead = ({match}) => {
   console.log(match)
   const showNews = news.filter(item => item.id === Number(match.params.id))
   .map(item => (
     <>
-    <div className="news__box news__read">
+    <div className="news__read-box">
       <div className="news__img-shadow">
-      <img src={item.img} alt="Pic" className="news__img-big" />
+      <img src={item.img} alt="Pic" className="news__img-picture" />
       </div>
       
-    <h1 className='news__title'>{item.title}</h1>
-    <div className="news__text-big">
-      <p>{item.description}</p>
+    <h1 className='news__read-title'>{item.title}</h1>
+    <div className="news__read-container">
+      {item.category === 'Coverage' 
+      ? 
+      item.description.map(desc => (
+        <>
+        <h4>{desc.t}</h4>
+        <p>{desc.p}</p>
+        </>
+      ))
+      : item.category === 'Interview' 
+      ?
+      item.description.map(desc => (
+        <>
+        <div className="news__text-interview">
+          <div className="news__text-interview-question"></div>
+        <p className="news__text-interview-question-text">{desc.q}</p>
+        </div>
+        <div className="news__text-interview">
+          <div className="news__text-interview-answer"></div>
+          <p className="news__text-interview-answer-text">{desc.a}</p>
+        </div>
+        
+        
+        </>
+      ))
+      : ''
+      
+      }
+
+      
     </div>
-   <div className="news__change">
-   <Link to={item.id>1 ? `/news/${item.id-1}` : `/news/`} className="news__change-prev">Prev
-  {news.filter(item => item.id === Number(match.params.id)-1).map(next => <h5 className="news__change-title">{next.title}</h5>)}
-   </Link>
-   <Link to={item.id!==news.length-1 ? `/news/${item.id+1}` : `/news/`} className="news__change-next">Next
-  {news.filter(item => item.id === Number(match.params.id)+1).map(next => <h5 className="news__change-title">{next.title}</h5>)}
-   </Link>
-   </div>
+      <div className="news__change">
+      <Link to={item.id>1 ? `/news/${item.id-1}` : `/news/`} className="news__change-prev">Prev
+      {news.filter(item => item.id === Number(match.params.id)-1).map(next => <h5 className="news__change-title">{next.title}</h5>)}
+      </Link>
+      <Link to={item.id!==news.length-1 ? `/news/${item.id+1}` : `/news/`} className="news__change-next">Next
+      {news.filter(item => item.id === Number(match.params.id)+1).map(next => <h5 className="news__change-title">{next.title}</h5>)}
+      </Link>
+      </div>
 
     </div>
    
--- src/data/news.js
+++ src/data/news.js
@@ -2,21 +2,52 @@ export const news = [
   {
     id: 1,
     img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Football_iu_1996.jpg/1200px-Football_iu_1996.jpg',
-    title: 'Amazing plays',
-    description: `According to research from the High Pay Centre, an independent think tank based in London, FTSE 100 chief executives earn a median of £3.6m ($4.9m) a year – more than 100 times the £31,461 earned by full-time employees. At the top of the pile of those CEOs is Tim Steiner, chief executive of the online supermarket Ocado, who was paid £58.7m in 2019. Thats 2,605 times the company's staff on average. In one day, he earned seven times their annual salary.
-    Across the Atlantic, the picture is even more extreme. Analysis by the Economic Policy Institute, a Washington DC-based think tank, showed chief executives of the 350 largest US companies earned an average $21.3m (£16.9m) in 2019. This puts the CEO-to-worker pay ratio at 320 to 1 – more than five times the level in 1989.
-    These findings come as the coronavirus pandemic has worsened inequality across the world, exposing low-income populations to greater health risks, job losses and declines in wellbeing. These divides have come into sharper focus than ever as awareness grows of the value of 'essential' workers – who often have few employment rights and little pay.
-    The result is mounting confusion and anger over the extraordinarily high salaries that top bosses continue to earn. With these deep-set inequalities laid bare, the question for many is how these huge pay packets ever came about. By whom and how they are given the green light and, crucially, should they continue to have a place in post-pandemic society?`
-  },
+    category: 'Coverage',
+    title: "First match",
+    description: [
+      {
+      t: 'First half',
+      p: 'This was one of the best match on this week'
+      },
+      {
+        t: 'Second half',
+        p: 'This was one of the best match on this week'
+      },
+    ]
+   },
   {
     id: 2,
     title: 'This will be the long title here for test longest of next prev change',
-    description: 'Oustragenius team'
+    category: 'Interview',
+    description: [
+      {
+      q: 'Tararala',
+      a: "rutututu"
+      },
+      {
+      q: 'Second question',
+      a: "rutututu"
+      },
+  ]
   },
   {
     id: 3,
-    title: 'Best Playes',
-    description: 'Oustragenius team'
+    title: 'Exclusive interview with coach',
+    category: 'Interview',
+    description: [
+      {
+      q: 'How do you feel right now?',
+      a: "Good! Pretty good. Honestly, it’s still not settled, still not really got it, but it feels brilliant. It was big and it was a wonderful experience. The whole ride is a wonderful experience but especially last night. It was very special. When people spoke about it and thought maybe it’s better to win it against City when we play them [next week] on the pitch, but honestly after the 97 points ride last year plus extension with the Champions League, and this season, and all the years before chasing the Champions League spot, I was thinking I want it as early as possible."
+      },
+      {
+      q: "How nice was it that you were all together last night? I’m not sure how much of a part you played in making it happen, but the group all able to enjoy that moment together",
+      a: "Look, so many people would have deserved to be part of this last night. So many more people than there were last night. But we had to make decisions. We all had to make decisions and the decision was everybody who is at Melwood in the moment with us every day and got tested twice can be there, no other people. So we did it without families of course, we did it without the wives as well. It was just the team and the staff around, but that was the minimum that we had to do. You couldn’t or I cannot imagine how it would have been had we been all alone at home. Yes, with our families but still kind of alone and not with the players together, how that would have felt. So it was perfect. It was perfectly organised. Nobody had a long time to organise it obviously. We decided to do it after the game against Crystal Palace, we will give it a try. It was big, a big emotional moment. The final whistle was a big moment. "
+      },
+      {
+        q: "Was it a kind of night when you spoke to the players or did you just want to see them celebrating?",
+        a: "I think it would have been appropriate to speak but I couldn’t. I just couldn’t. I realised when I started doing the Sky TV interview when the tears came up and then I spoke to Ulla. I had no chance to say a word, I just cried and if I’d have spoken to the players they wouldn’t understand me, so it was not the right moment to do. I have enough opportunities to tell the boys what I think about them and how thankful I am and how much I appreciate their effort they put in, the determination, desire, everything. But they should understand me in that moment and it was not possible last night."
+      }
+  ]
   },
   {
     id: 4,
