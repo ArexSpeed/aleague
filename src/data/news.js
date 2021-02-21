0631e63b9c440888eb8b142fe288a04a51aa2296
Arek Cichocki <arexspeed95@gmail.com>
2021-02-21 21:09:14 +0100

    add news data

--- src/data/news.js
+++ src/data/news.js
@@ -3,74 +3,134 @@ export const news = [
     id: 1,
     img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Football_iu_1996.jpg/1200px-Football_iu_1996.jpg',
     category: 'Coverage',
-    title: "First match",
+    title: "Half Season Summery",
     description: [
       {
-      t: 'First half',
-      p: 'This was one of the best match on this week'
+      t: 'We are after 15 rounds of the season, that means we are on the half of the way to meet the champion of 2021.',
+      p: `We saw a lot of emotional games and this season is really special.
+      For the first time the leader is Arkov Diamod, a team which is built on young players from academy and I think everyone enjoys their game. As leader the didn't lost any game, won 11 and draw 4 times what gives 37 points. 
+      `
       },
       {
-        t: 'Second half',
-        p: 'This was one of the best match on this week'
+        t: 'Chasing the leader',
+        p: `Very close are Shamrock with 33 points and they lost only 1 games which still makes them very dangerous rival.
+        Over 30 points have also Diampero and Angelo Angels and both conceded the fewest goals in the league only 6.
+        Current champion Victory Tigers are 6 but we have to remember they are always dangerous and unpredictable. Their games are so emotional and gripping to the last second. They lost only 2 games but they draw as many as six.
+        `
+      },
+      {
+        t: 'Big disappointment',
+        p: `The most disappointing team is Dark Knight. Before the season they were one of the favorites, last season they were fourth with medals aspiration. In this season they won only 2 games and drew 6 and they are only 13 in the table. Maybe in the second part they will overcome the crisis and back to their level.
+        `
+      },
+      {
+        t: 'The red lanterns',
+        p: 'On the bottom of the table are Kingsleen Monarch with only one win and 4 draws and Ronnpolis FC which doesn’t win any match and score only 5 goals. They are in a very big crisis and if they will not face it they will probably finish the league on the bottom.'
+      },
+      {
+        t: 'Players stats',
+        p: `The best scorer on the half is George Gegree with 12 goals and 3 assists. He is the real leader of Diamond with shares of 15 goals from 24. In this form he should win this ranking, but he has to watch out for the second in classification Jack Adams who has 11 goals.
+        The best assistant second season in row is Tim Arkovsky with 8 assists and 8 goals is even the best in the summary classification. The brain of Shamrock is on another level and his quality is a great advantage of his club. 
+        In an infamous cards classification the first is Tales Milan who collected as many as 3 red cards which show us how big a mess is inside the worst current team Ronnpolis.
+        `  
       },
     ]
    },
   {
     id: 2,
-    title: 'This will be the long title here for test longest of next prev change',
+    title: 'Exclusive interview with Diamonds coach',
     category: 'Interview',
     description: [
       {
-      q: 'Tararala',
-      a: "rutututu"
+      q: 'Good morning. You are the leader after the first half of the season. How do you feel?',
+      a: "Hello. Yes this is an amazing score. We worked really hard before the season and now we have the results. "
       },
       {
-      q: 'Second question',
-      a: "rutututu"
+      q: 'Diamonds has not won any medal for ten years and if we remind last four seasons when your team was in the second ten then your current results shocked many experts',
+      a: "Yes that is true. Last seasons have not been easy for us because we had a thorough reconstruction of the team. We gave young players a chance even if we didn’t have a result for a while but our team was a bit old and we had to take steps to rejuvenate the staff."
       },
+      {
+      q: "After 2016 you lost 6 main players. Any team didn’t have such a difficult situation. Why did you choose to bet players from the Academy instead of buying a few class players?      ",
+      a: "This process had been going on for a long time because years ago club management invested a lot of money to build one of the best Academy in the league and we knew that day when this player would be ready to play at a higher level would finally come. Situation from 2016 just accelerated this process.      "
+      },
+      {
+      q: "Exactly and now we can see that the risk was worth it. You made new football stars.      ",
+      a: "I wouldn’t say it was a risk because most of them were a Junior League Champion. We known that if we give them a chance they will be able to get something great. But we had to be patient and now you are right they are stars but we have to remember they are still young and they still can be better.      "
+      },
+      {
+      q: "If they are young how do you think, will you be able to keep this good level?      ",
+      a: " I hope but now it’s only half of the season and it is too early to judge anything. We are ready to fight.      "
+      },
+      {
+        q: "Your series are incredible, because you haven’t lost any matches yet. Are you going for a record and don’t lose any games?        ",
+        a: "I don't even know the actual record but usually the champion won the most meetings. For now we have to be focus on the next game and hard work every training session then maybe we can break the record but I will be satisfied when our players will stick to our tactic and will not make mistakes."
+        },
+        {
+        q: "Are you planning any transfers in this half season break or you are good with the current squad?        ",
+        a: "No, we are lucky that our players are healthy and are in good shape. We don’t need any new players because for now our squad is wide and even if something will happen we can next ambitious players who only wait for a chance.        "
+        },
+        {
+        q: "Thank you for this conversation and I wish you all the best and maybe in a half year I will be able to call you a champion.",
+        a: "Thanks also too, I hope your wish will come true."
+        },
   ]
   },
   {
     id: 3,
-    title: 'Exclusive interview with coach',
-    category: 'Interview',
-    description: [
-      {
-      q: 'How do you feel right now?',
-      a: "Good! Pretty good. Honestly, it’s still not settled, still not really got it, but it feels brilliant. It was big and it was a wonderful experience. The whole ride is a wonderful experience but especially last night. It was very special. When people spoke about it and thought maybe it’s better to win it against City when we play them [next week] on the pitch, but honestly after the 97 points ride last year plus extension with the Champions League, and this season, and all the years before chasing the Champions League spot, I was thinking I want it as early as possible."
-      },
-      {
-      q: "How nice was it that you were all together last night? I’m not sure how much of a part you played in making it happen, but the group all able to enjoy that moment together",
-      a: "Look, so many people would have deserved to be part of this last night. So many more people than there were last night. But we had to make decisions. We all had to make decisions and the decision was everybody who is at Melwood in the moment with us every day and got tested twice can be there, no other people. So we did it without families of course, we did it without the wives as well. It was just the team and the staff around, but that was the minimum that we had to do. You couldn’t or I cannot imagine how it would have been had we been all alone at home. Yes, with our families but still kind of alone and not with the players together, how that would have felt. So it was perfect. It was perfectly organised. Nobody had a long time to organise it obviously. We decided to do it after the game against Crystal Palace, we will give it a try. It was big, a big emotional moment. The final whistle was a big moment. "
-      },
-      {
-        q: "Was it a kind of night when you spoke to the players or did you just want to see them celebrating?",
-        a: "I think it would have been appropriate to speak but I couldn’t. I just couldn’t. I realised when I started doing the Sky TV interview when the tears came up and then I spoke to Ulla. I had no chance to say a word, I just cried and if I’d have spoken to the players they wouldn’t understand me, so it was not the right moment to do. I have enough opportunities to tell the boys what I think about them and how thankful I am and how much I appreciate their effort they put in, the determination, desire, everything. But they should understand me in that moment and it was not possible last night."
-      }
-  ]
-  },
-  {
-    id: 4,
-    title: 'Greatest player',
+    title: "Leaders show 3-1 Arkov Diamond - Tozen Goats / Georgee’s show",
     category: 'Coverage',
     description: [
       {
-      t: 'First half',
-      p: 'This was one of the best match on this week'
+      t: "Big show on Arkovia Arena",
+      p: "In the last game of the 15 round Arkov Diamond hosted Tozen Goats and as predicted they won 3:1. But this game was a total show of one player. Leader Diamonds Georg Gorgee scored a hat-trick. The stricker quickly opened the score in the 10 minute he shot for the first time. "
       },
       {
-        t: 'Second half',
-        p: 'This was one of the best match on this week'
+        t: "",
+        p: `Few minutes ago he signed up to the shooter list for the second time. From right side Tom Innik played perfectly on the striker’s head and he shot over the goalkeeper’s hand, shot no to save. Guest was trying to go back to the game but Armin Soler shot right next to the gate.
+        In the second half Goat’s coach made two changes and their play looked a little better. In 55 minute players from Tozen managed to defeat the hosts goalkeeper and the score was 2:1. However after this goal the host only increased their domination and in 10 minutes they did 10 shots, and of course one of these was the third goal of George Gorgee. After game the hosts strengthened on the leader position and thank 3 goals Gorgee is a leader of shooter in this season.`
       },
+
     ]
   },
   {
-    id: 5,
-    title: 'Greatest player',
+    id: 4,
+    title: 'Exclusive interview with one of the main referee Paul Bertney',
     category: 'Interview',
-    description: [{
-      q: 'Tararala',
-      a: "rutututu"
-    }]
+    description: [
+      {
+      q: 'You started working as a referee 15 years ago. Why did you choose this profession?',
+      a: "When I was teenager I was playing in a local club, then I went into the university team. But when I graduated High School I realised that I can’t have the chance and so much talent to play in one of the clubs in primary league. But I still loved football and it was always my passion, so I thought signing to the referee course could be a great idea."  
+    },
+    {
+      q: `5 years later you start refereeing your first ALeague game. Your example shows that never give up on your dreams.`,
+      a: `We have to realise that football teaches humility. The starting point should be that anyone who wants to become a football player or referee has to love football. Remember that a football game needs a referee to start. He is a permanent element of the game.In referee you have to understand that there is no easy situation sometimes you have to make a difficult decision. The function of arbitrator develops various human traits, such as quick decision making, people management or coping with work under conditions of stress and time pressure. This is useful not only on the pitch but also in life.
+      `
+    },
+    {
+      q: `So we can say that work as referee is a forge for character
+      `,
+      a: `Of course. From a player's view you look at football through the prism of the will to win. For referees the most important is that the competition follows strictly defined rules. This teaches discipline. In addition, referee training means that you always maintain a proper physical condition, you have to pass exams and train regularly. Moreover it is easier to stay fit in a group of referees. You can also learn more about training secrets and methods of regeneration. It is a constant exchange of information as well as interesting acquaintances and friendship that can remain for life.
+      `
+    },
+    {
+      q: `The continental cup will take place next year. Is your goal to be in the group of referees at this championship?
+      `,
+      a: `I prefer to set myself short-term goals. The most important thing is to keep form and health. The more games you referee, the more chances you have to take part in the tournament. I would like refereeing to still be fun and if I will be good enough I can get a chance to appear on the biggest ares.
+      `
+    },
+
+  ]
   },
+  {
+    id: 5,
+    title: "Diampero Bolts is finalizing the transfer. They play about 70 millions.",
+    category: 'Coverage',
+    description: [
+      {
+      t: "New faces on Bolts Area",
+      p: "Diampero is arming before the second part of the season and they make third reinforcement.  The third team in the table will join 19-years old striker Piter Lerke. Value of this player looks very high but he is a big talent. At this moment he is a top scorer in Junior League and 4 clubs fought for his services. Finally he will join the northern team. He is 19 years old and plays as a central striker but he is also flexible and he can play also on the wings. This will be useful for tactics. The transfer talks are nearing completion and player should start medical tests in a few days. Let us remind you that this is the third transfer to the Bolts teams. After strengthening the defence they want to add quality to attack. It heralds that Diampero wants to join the fight for the championship. "
+      },
+    ]
+  },
+  
 ]
