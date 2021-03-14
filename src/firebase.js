7c39ae440e326d312b78b52599696ab3ff11ad6b
Arek Cichocki <arexspeed95@gmail.com>
2021-03-14 23:05:32 +0100

    fierbase

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB_RDcTn1Bk0uwrWQ2hkJ40eVPjf9hot0U",
+  authDomain: "alleague.firebaseapp.com",
+  projectId: "alleague",
+  storageBucket: "alleague.appspot.com",
+  messagingSenderId: "842319460808",
+  appId: "1:842319460808:web:174b63a7f05527539128fd",
+  measurementId: "G-80ZXPVN4S8",
+};
